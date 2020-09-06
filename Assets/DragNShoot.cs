995a845599034ce70badf7cade258188c7ce0a2c
damar <damar@gitlab.lab>
2020-09-06 19:51:40 +0700

    create simple slingshot

---
+++ Assets/DragNShoot.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// https://www.youtube.com/watch?v=Xu0urrCBSpw&t=45s thanks for Tyler Potts
+public class DragNShoot : MonoBehaviour
+{
+    public float power = 10f;
+    public Rigidbody2D rb;
+
+    public Vector2 minPower;
+    public Vector2 maxPower;
+
+    Camera cam;
+    Vector2 force;
+
+    TrajectoryLine tl;
+
+    Vector3 startPoint;
+    Vector3 endPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+        tl = GetComponent<TrajectoryLine>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        if (Input.GetMouseButtonDown(0)) {
+            startPoint = cam.ScreenToWorldPoint(Input.mousePosition);
+            startPoint.z = 15f;
+        }
+
+        if (Input.GetMouseButton(0)) {
+            Vector3 currentPoint = cam.ScreenToWorldPoint(Input.mousePosition);
+            currentPoint.z = 15f;
+            
+            tl.RenderLine(transform.position, new Vector3(pos.x + (pos.x - 2*currentPoint.x), pos.y + (pos.y - 2*currentPoint.y)));
+        }
+
+        if (Input.GetMouseButtonUp(0)) {
+            endPoint = cam.ScreenToWorldPoint(Input.mousePosition);
+            endPoint.z = 15f;
+
+            force = new Vector2(Mathf.Clamp(pos.x - pos.x + (pos.x - 2*endPoint.x), minPower.x, maxPower.x), Mathf.Clamp(pos.y - pos.y + (pos.y - 2*endPoint.y), minPower.y, maxPower.y));
+            rb.AddForce(force * power, ForceMode2D.Impulse);
+            tl.EndLine();
+        }
+    }
+}
---
+++ Assets/TrajectoryLine.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// https://www.youtube.com/watch?v=Xu0urrCBSpw&t=45s thanks for Tyler Potts
+[RequireComponent(typeof(LineRenderer))]
+public class TrajectoryLine : MonoBehaviour
+{
+    public LineRenderer lr;
+    private void Awake() {
+        lr = GetComponent<LineRenderer>();
+    }
+    private void Start() {
+        EndLine();
+    }
+    public void RenderLine(Vector3 startPoint, Vector3 endPoint) {
+        lr.positionCount = 2;
+        Vector3[] points = new Vector3[2];
+        points[0] = startPoint;
+        points[1] = endPoint;
+
+        lr.SetPositions(points);
+    }
+
+    public void EndLine() {
+        lr.positionCount = 0;
+    }
+}
